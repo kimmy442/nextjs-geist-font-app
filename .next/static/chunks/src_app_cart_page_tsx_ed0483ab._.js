780550dc9f91d485a7e712e80bd7a30d38b66a16
kimmy442 <kimcardash7@gmail.com>
2025-07-19 04:01:02 -0500

    Add .next/static/chunks/src_app_cart_page_tsx_ed0483ab._.js 1752915664065

---
+++ .next/static/chunks/src_app_cart_page_tsx_ed0483ab._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_6f8f3344._.js",
+  "static/chunks/src_ec87e82c._.js"
+],
+    source: "dynamic"
+});
