f891d860d5be5af85c03549f14b6dc28aa6f7ffb
kimmy442 <kimcardash7@gmail.com>
2025-07-19 04:01:00 -0500

    Add .next/static/chunks/src_app_products_[productId]_page_tsx_ed0483ab._.js 1752915662504

---
+++ .next/static/chunks/src_app_products_[productId]_page_tsx_ed0483ab._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_73707f88._.js",
+  "static/chunks/src_ddef7910._.js"
+],
+    source: "dynamic"
+});
